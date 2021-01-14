7c8741195887d88dd70b0db6a509680d7e686603
johnb8005 <johnb8005@users.noreply.github.com>
2021-01-14 22:47:45 +0000

    deploy: f6f0d2fc64ff114d2cfe05c03d5ceae7d423296e

---
+++ precache-manifest.b397141bc28839fdd323c9eb28d3abb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939ce3be25eaa8c062b0ef38fd530371",
+    "url": "/tableau-wdc-react/index.html"
+  },
+  {
+    "revision": "6e2b065c18eff5be245a",
+    "url": "/tableau-wdc-react/static/js/2.9886d967.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tableau-wdc-react/static/js/2.9886d967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e140ab5a26f652ebebb",
+    "url": "/tableau-wdc-react/static/js/main.de1bdafd.chunk.js"
+  },
+  {
+    "revision": "ce8d44834b27a268ebba",
+    "url": "/tableau-wdc-react/static/js/runtime-main.938f11fc.js"
+  }
+]);
